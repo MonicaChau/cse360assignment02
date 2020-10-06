4169cfb4e60495ea02d3608f6dad3ba9d32c30e3
Monica Chau <MonicaChau@Monicas-MacBook-Pro.local>
2020-10-05 18:13:15 -0700

    Revert "Revert "Update AddingMachine.java""
    This reverts commit 2b3f6a21f82f4b9eaf58c9e2a60525ac1cfd1af8.

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,29 +1,34 @@
 package cse360assignment02;
 
 public class AddingMachine {
+	
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history + " - " + value;
 	}
 	
 	public String toString () {
-		return "";
+		System.out.println(history);
+		return history;
 	}
 	
 	public void clear() {
-		
+		history = "0";
 	}
 }
